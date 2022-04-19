da40cd6d7b287e9db1c1f807b64a869a7fdeddb4
bayustrio7 <bayustrio7@gmail.com>
2022-04-19 10:03:06 +0700

    feat: updates load

--- src/pages/Hadits/index.js
+++ src/pages/Hadits/index.js
@@ -3,68 +3,73 @@ import axios from "axios";
 import "./hadits.css";
 import { BsArrowDownShort } from "react-icons/bs";
 // import slug from "./HaditSlug";
-import { useParams } from "react-router-dom";
+import { Spin } from "antd";
 import { Link } from "react-router-dom";
-import {useDispatch,useSelector} from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { getDataSlug } from "../../React-redux/Redux-action/Action";
 
 const Hadits = () => {
-    const count = useSelector((state) => state.count)
-  const Slug = useSelector((state) => state.slug)
-
-    const dispatch = useDispatch();
+  const Slug = useSelector((state) => state.slug);
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
   const [data, setData] = useState([]);
   const [slide, setSlide] = useState(false);
-  const [id, setId] = useState('');
-  const params = useParams();
+  const [id, setId] = useState("");
 
   useEffect(() => {
-
     let API = "https://hadis-api-id.vercel.app/hadith/";
     axios.get(API).then((res) => {
       console.log(res.data);
       setData(res.data);
+      setLoading(true);
     });
   }, []);
 
   const handleClick = (slug) => {
-    console.log(slug,' << INDEX HADITS');
+    console.log(slug, " << INDEX HADITS");
     dispatch(getDataSlug(slug));
   };
 
   const handleSlide = (idx) => {
-      setSlide(!slide);
-      setId(idx)
-    console.log(idx)
-  }
+    setSlide(!slide);
+    setId(idx);
+    console.log(idx);
+  };
 
   return (
     <>
       <div>
-          <h1>{Slug}</h1>
-        {data.map((item, idx) => (
-          <div key={idx} className="section-hadits">
-            <div className="text-perawi">
-              <div className={slide ? "active" : "text-hadits"}>
+        <h1>{Slug}</h1>
+        {loading ? (
+          data.map((item, idx) => (
+            <div key={idx} className="section-hadits">
+              <div className="text-perawi">
+                <div className={slide ? "active" : "text-hadits"}>
                   <h2 onClick={() => handleClick(item.slug)}>
-                <Link style={{textDecoration:'none'}} key={item.slug} 
-                to={`/hadits/${item.slug}`}
-                >
-                  {item.name}
-                </Link>
-                </h2>
-                {id === idx &&
-                <p key={idx}>Total Hadits: {item.total}</p> 
-                }
-              </div>
-              <div className="arrow" onClick={() => handleSlide(idx)}>
-                <p>
-                  <BsArrowDownShort />
-                </p>
+                    <Link
+                      style={{ textDecoration: "none" }}
+                      key={item.slug}
+                      to={`/hadits/${item.slug}`}
+                    >
+                      {item.name}
+                    </Link>
+                  </h2>
+                  {id === idx && <p key={idx}>Total Hadits: {item.total}</p>}
+                </div>
+                <div className="arrow" onClick={() => handleSlide(idx)}>
+                  <p>
+                    <BsArrowDownShort />
+                  </p>
+                </div>
               </div>
             </div>
+          ))
+        ) : (
+          <div style={{ width: "100%", fontSize: "4rem" }}>
+            <Spin style={{ width: "100%", fontSize: "10rem" }} size="large" />
+            <h4 style={{ textAlign: "center" }}>Loading...</h4>
           </div>
-        ))}
+        )}
       </div>
     </>
   );
